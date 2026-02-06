bdf50a6d1a64bb7e0ce720e8d7cca9db106e108a
Shreyas Bidadi <sbidadi@kl1.head.cm.kestrel.hpc.nrel.gov>
2026-02-06 10:08:00 -0700

    Changes to make sure the trained temporal model's path is correctly set

--- lib/runners.py
+++ lib/runners.py
@@ -453,7 +453,7 @@ class latentRunner(nn.Module):
                         "history":history,
                         "time":cost_time}
         
-        torch.save(check_point,pathsBib.model_path + self.filename + self.fmat)
+        torch.save(check_point,pathsBib.pretrain_path + self.filename + self.fmat)
         print(f"INFO: The checkpoints has been saved!")
 
 #-------------------------------------------------
@@ -474,7 +474,7 @@ class latentRunner(nn.Module):
         assert(model_type in model_type_all), print('ERROR: No type of the model matched')
 
         if      model_type == 'pre':    model_path = pathsBib.pretrain_path + self.filename               + self.fmat
-        elif    model_type == 'val' :   model_path = pathsBib.chekp_path    + self.filename + self.fmat
+        elif    model_type == 'val' :   model_path = pathsBib.pretrain_path + self.filename + self.fmat
         elif    model_type == 'final' : model_path = pathsBib.chekp_path    + self.filename + '_final'    + self.fmat
         try:
             ckpoint = torch.load(model_path, map_location= self.device)
